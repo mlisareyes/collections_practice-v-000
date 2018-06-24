6411fc37a5a05842177daa661496bfae941690c5
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-24 04:34:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@ def sort_array_asc(numbers)
   numbers.sort
 end
 
-def sort_array_desc(down)
-  down.sort! {|x, y| y <=> x}
-  puts down
+def sort_array_desc(num)
+  num.sort do {|x, y| y <=> x}
+  puts num
 end
